3191aeb0e782d6ab005a63b41c69e2f7df3f7693
Zacharias Bahm <zakbahm@gmail.com>
2022-12-14 01:28:33 -0500

    add material ui

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,11 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
